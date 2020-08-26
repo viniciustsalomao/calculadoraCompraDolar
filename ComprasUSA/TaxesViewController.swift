cfd93c8af684526573d77bba9cfc6e5fddfc8891
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-08-26 20:54:27 -0300

    Criação do projeto

---
+++ ComprasUSA/TaxesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaxesViewController.swift
+//  ComprasUSA
+//
+//  Created by Vinícius Tinajero Salomão on 25/08/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import UIKit
+
+class TaxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
